8ec1a65bd486c089552e0478e0d82e1cc89d951a
Ryan <ryanfelipe.rr@gmail.com>
2025-07-23 22:59:20 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,50 @@
+import express from 'express';
+import nodemailer from 'nodemailer';
+import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.post('/pedido', async (req, res) => {
+  const { email } = req.body;
+
+  if (!email) {
+    return res.status(400).json({ error: 'Email é obrigatório' });
+  }
+
+  try {
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: process.env.MAIL_USERNAME,
+        pass: process.env.MAIL_PASSWORD,
+      },
+    });
+
+    await transporter.sendMail({
+      from: process.env.MAIL_FROM_ADDRESS,
+      to: email,
+      subject: "Toc Toc, pedido Especial...",
+      text: "Eu te amo, minha fofinha! Tá afim de viver uma vida cheia de aventuras junto comigo? 💌",
+    });
+
+    res.status(200).json({ message: 'E-mail enviado com sucesso!' });
+  } catch (error) {
+    console.error('Erro ao enviar e-mail:', error);
+    res.status(500).json({ error: 'Erro ao enviar o e-mail.' });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
